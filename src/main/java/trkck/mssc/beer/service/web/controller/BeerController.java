a7969282a3e46bb04e9d3532568ea4a0b86b2d45
trkck <trk.kck95@gmail.com>
2025-03-15 02:38:13 +0300

    added error handler

--- src/main/java/trkck/mssc/beer/service/web/controller/BeerController.java
+++ src/main/java/trkck/mssc/beer/service/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package trkck.mssc.beer.service.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import trkck.mssc.beer.service.web.model.BeerDto;
 
@@ -18,12 +19,12 @@ public class BeerController {
         return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
         //to do implement
         return new ResponseEntity(HttpStatus.CREATED);
     }
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId,@Validated @RequestBody BeerDto beerDto){
 
         //to do Impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
